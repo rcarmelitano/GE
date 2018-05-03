a9e0dda119d0994a998ce054ce0c24abf2e1f809
jb0196 <jb0196@G005B-STATION08>
2018-05-02 20:36:23 -0400

    Finished Departments, almost done with Categories, started on Shrinkage

---
+++ GroceryGUI/AddCategories.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAddAndUpdateCategories
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.BackToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtCategoryName = New System.Windows.Forms.TextBox()
+        Me.btnCreateCategory = New System.Windows.Forms.Button()
+        Me.txtDepartmentID = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtCategoryID = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BackToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(367, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'BackToolStripMenuItem
+        '
+        Me.BackToolStripMenuItem.Name = "BackToolStripMenuItem"
+        Me.BackToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.BackToolStripMenuItem.Text = "Back"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(60, 121)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Category Name:"
+        '
+        'txtCategoryName
+        '
+        Me.txtCategoryName.Location = New System.Drawing.Point(149, 118)
+        Me.txtCategoryName.Name = "txtCategoryName"
+        Me.txtCategoryName.Size = New System.Drawing.Size(153, 20)
+        Me.txtCategoryName.TabIndex = 3
+        '
+        'btnCreateCategory
+        '
+        Me.btnCreateCategory.Location = New System.Drawing.Point(95, 165)
+        Me.btnCreateCategory.Name = "btnCreateCategory"
+        Me.btnCreateCategory.Size = New System.Drawing.Size(177, 49)
+        Me.btnCreateCategory.TabIndex = 5
+        Me.btnCreateCategory.Text = "Create Category"
+        Me.btnCreateCategory.UseVisualStyleBackColor = True
+        '
+        'txtDepartmentID
+        '
+        Me.txtDepartmentID.Location = New System.Drawing.Point(149, 81)
+        Me.txtDepartmentID.Name = "txtDepartmentID"
+        Me.txtDepartmentID.Size = New System.Drawing.Size(153, 20)
+        Me.txtDepartmentID.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(64, 84)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Department ID:"
+        '
+        'txtCategoryID
+        '
+        Me.txtCategoryID.Enabled = False
+        Me.txtCategoryID.Location = New System.Drawing.Point(149, 45)
+        Me.txtCategoryID.Name = "txtCategoryID"
+        Me.txtCategoryID.ReadOnly = True
+        Me.txtCategoryID.Size = New System.Drawing.Size(153, 20)
+        Me.txtCategoryID.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(77, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Category ID:"
+        '
+        'frmAddAndUpdateCategories
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(367, 226)
+        Me.Controls.Add(Me.txtCategoryID)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtDepartmentID)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnCreateCategory)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtCategoryName)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmAddAndUpdateCategories"
+        Me.Text = "Add a Category"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents BackToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtCategoryName As TextBox
+    Friend WithEvents btnCreateCategory As Button
+    Friend WithEvents txtDepartmentID As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtCategoryID As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ GroceryGUI/CategoriesAndDepartmentsMenu.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCategoriesAndDepartments
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCategories = New System.Windows.Forms.Button()
+        Me.btnDepartments = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.mnuBack = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuHelp = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnCategories
+        '
+        Me.btnCategories.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.btnCategories.Location = New System.Drawing.Point(72, 171)
+        Me.btnCategories.Name = "btnCategories"
+        Me.btnCategories.Size = New System.Drawing.Size(132, 56)
+        Me.btnCategories.TabIndex = 6
+        Me.btnCategories.Text = "Categories"
+        Me.btnCategories.UseVisualStyleBackColor = True
+        '
+        'btnDepartments
+        '
+        Me.btnDepartments.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.btnDepartments.Location = New System.Drawing.Point(72, 61)
+        Me.btnDepartments.Name = "btnDepartments"
+        Me.btnDepartments.Size = New System.Drawing.Size(132, 56)
+        Me.btnDepartments.TabIndex = 5
+        Me.btnDepartments.Text = "Departments"
+        Me.btnDepartments.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuBack, Me.mnuHelp})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(282, 24)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'mnuBack
+        '
+        Me.mnuBack.Name = "mnuBack"
+        Me.mnuBack.Size = New System.Drawing.Size(44, 20)
+        Me.mnuBack.Text = "Back"
+        '
+        'mnuHelp
+        '
+        Me.mnuHelp.Name = "mnuHelp"
+        Me.mnuHelp.Size = New System.Drawing.Size(44, 20)
+        Me.mnuHelp.Text = "Help"
+        '
+        'frmCategoriesAndDepartments
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 266)
+        Me.Controls.Add(Me.btnCategories)
+        Me.Controls.Add(Me.btnDepartments)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "frmCategoriesAndDepartments"
+        Me.Text = "CategoriesAndDepartmentsMenu"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCategories As Button
+    Friend WithEvents btnDepartments As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents mnuBack As ToolStripMenuItem
+    Friend WithEvents mnuHelp As ToolStripMenuItem
+End Class
